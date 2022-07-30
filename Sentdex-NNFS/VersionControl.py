23f26a584358a8f4d4722858b68719a4b352f8c1
Huseyinc <enghcinar@gmail.com>
2022-07-31 00:21:34 +0300

    NNFS Update

--- Sentdex-NNFS/VersionControl.py
+++ Sentdex-NNFS/VersionControl.py
@@ -13,6 +13,6 @@ import nnfs
 print("Python: ", sys.version) #must > 3.7.7
 print("Numpy:", np.__version__) #must > 1.18.2
 print("Matplotlib:", matplotlib.__version__) #must >3.2.1
-print("NNFS:", nnfs.__version__)  #0.5.1 tutorials codes
+print("NNFS:", nnfs.__version__)  #could be > 0.5.1  
 #Version Info End
 
