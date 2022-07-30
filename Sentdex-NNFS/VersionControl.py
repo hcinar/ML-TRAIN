37f61f268f72e6c97240f4cf281081440426a509
Huseyinc <enghcinar@gmail.com>
2022-07-31 00:20:09 +0300

    Activation Method ReLU

--- Sentdex-NNFS/VersionControl.py
+++ Sentdex-NNFS/VersionControl.py
@@ -6,11 +6,13 @@ What versions we need to complete series
 import sys
 import numpy as np
 import matplotlib
+import nnfs
 #Imports End
 
 #Version Info Start
 print("Python: ", sys.version) #must > 3.7.7
 print("Numpy:", np.__version__) #must > 1.18.2
 print("Matplotlib:", matplotlib.__version__) #must >3.2.1
+print("NNFS:", nnfs.__version__)  #0.5.1 tutorials codes
 #Version Info End
 
