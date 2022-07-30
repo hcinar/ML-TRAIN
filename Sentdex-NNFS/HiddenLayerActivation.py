cd6bc3f868972b2881f6082ecabc829756aa0975
Huseyinc <enghcinar@gmail.com>
2022-07-31 01:02:05 +0300

    commets added.

--- Sentdex-NNFS/HiddenLayerActivation.py
+++ Sentdex-NNFS/HiddenLayerActivation.py
@@ -3,12 +3,15 @@ import numpy as np
 import nnfs 
 from nnfs.datasets import spiral_data
 
+#Neural Network From Scratch Initialization
 nnfs.init()
 
 #Input Data Sets
+#2 Inputs Batch size 100
 X, y = spiral_data(100, 3)
 
 
+#Class for Neuron Calculation
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):
         self.weights = 0.10*np.random.rand(n_inputs,n_neurons)
@@ -16,15 +19,18 @@ class Layer_Dense:
     def forward(self,inputs):
         self.output = np.dot(inputs,self.weights)+self.biases
 
+#Class for Activation of Inputs
 class Activation_ReLU:
     def forward(self,inputs):
         self.output = np.maximum(0,inputs)
 
-
+#Layer with 2 Inputs and 5 Neuron
 layer1 = Layer_Dense(2,5)
+#Activation of 5 Neuron
 activation1 = Activation_ReLU()
 
+#Input Batch into Layer1
 layer1.forward(X)
-print(layer1.output)
+#Activate Layer1 Output
 activation1.forward(layer1.output)
 print(activation1.output)
