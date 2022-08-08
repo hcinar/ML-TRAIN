6ef9bd52abf52a19bb8540f559ba352a2a541934
Huseyinc <enghcinar@gmail.com>
2022-08-08 23:09:27 +0300

    get historical data from binance

---
+++ Neural Network for Finance/0001_BA_WeeklyHistoricalDataToExcel.py
@@ -0,0 +1,54 @@
+#Imports
+from binance.client import Client
+import pandas as pd
+
+
+#KeySecretPath --> key,secret
+key_file_path = r'/home/hus/GIT/ML-TRAIN/Neural Network for Finance/key-secret.txt'
+
+#API DEFAULTS
+RECV_WINDOW=1000
+
+#STATIC FIELDS
+key, secret = '',''
+
+#SYMBOL
+symbol = "BTCUSDT"
+from_date = "1 Jan, 2010"
+
+#Export Path
+data_export_path = r"/home/hus/GIT/ML-TRAIN/Neural Network for Finance/"+symbol+"W"+'.xlsx'
+
+#Check Client Condition
+def CheckClient(_client):
+    #get client condition #200 OK.
+    client_condition = False
+    if str(_client.response).lower().count('200') > 0 :
+        client_condition = True
+        response_time = _client.get_server_time()
+        print("Response Time:",response_time)
+    if not client_condition:
+        print("Server gets an connection error.\n Response:",_client.response)
+        quit()
+
+#Get keys from file
+def GetKeysFromFile(key_file_path):
+    with open(key_file_path) as f:
+        line = f.readline()
+        key,secret = line.split(',')
+    return key,secret
+
+#Get weekly data from file
+def GetWeeklyDataToExcel(symbol, from_date, data_export_path, client):
+    # fetch weekly klines since it from_Date into excel file
+    weekly = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1WEEK, from_date)
+    df = pd.DataFrame(weekly)
+    df.to_excel(data_export_path)
+    return weekly
+    
+
+key, secret = GetKeysFromFile(key_file_path)
+#Client Access
+client = Client(key, secret)
+CheckClient(client)
+GetWeeklyDataToExcel(symbol, from_date, data_export_path, client)
